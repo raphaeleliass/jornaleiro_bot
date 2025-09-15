b621ee6f8ea24aec0dc519b91b50cd279e115264
Raphael Elias <raphaeleliass@outlook.com>
2025-09-15 11:07:01 -0300

    feat: added types for bot schema and telegram payload

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,3 +3,5 @@ import { drizzle } from "drizzle-orm/bun-sqlite";
 if (!process.env.DB_FILE_NAME) throw new Error("Missing DB_FILE_NAME variable");
 
 export const db = drizzle(process.env.DB_FILE_NAME);
+
+export type DrizzleDB = typeof db;
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,18 +1,54 @@
-export interface MessageInterface {
-	message: {
-		message_id: number;
-		from: {
-			id: number;
-			is_bot: boolean;
-			first_name: string;
-			language_code: string;
-		};
-		chat: {
-			id: number;
-			first_name: string;
-			type: string;
-		};
-		date: number;
-		text: string;
-	};
+export interface TelegramUpdate {
+	update_id: number;
+	message?: Message;
+	edited_message?: Message;
+	my_chat_member?: ChatMemberUpdated;
+	chat_member?: ChatMemberUpdated;
+}
+
+export interface Message {
+	message_id: number;
+	from?: User;
+	sender_chat?: Chat;
+	chat: Chat;
+	date: number;
+	text?: string;
+	new_chat_members?: User[];
+	left_chat_member?: User;
+}
+
+export interface User {
+	id: number;
+	is_bot: boolean;
+	first_name: string;
+	username?: string;
+	language_code?: string;
+}
+
+export interface Chat {
+	id: number;
+	type: "private" | "group" | "supergroup" | "channel";
+	title?: string;
+	username?: string;
+	first_name?: string;
+	last_name?: string;
+}
+
+export interface ChatMemberUpdated {
+	chat: Chat;
+	from: User;
+	date: number;
+	old_chat_member: ChatMember;
+	new_chat_member: ChatMember;
+}
+
+export interface ChatMember {
+	user: User;
+	status:
+		| "creator"
+		| "administrator"
+		| "member"
+		| "restricted"
+		| "left"
+		| "kicked";
 }
