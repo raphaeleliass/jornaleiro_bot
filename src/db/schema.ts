27ec9b1dbe9b48083e6fef71b6bc331f97dfe66d
Raphael Elias <raphaeleliass@outlook.com>
2025-09-15 11:03:10 -0300

    feat: implemented newsTable and some new fields on userTable

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,32 @@
+import { sql } from "drizzle-orm";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const user = sqliteTable("users_table", {
+export const userTable = sqliteTable("users", {
 	id: int().primaryKey({ autoIncrement: true }),
 	name: text().notNull(),
 	chatId: int("chat_id").notNull().unique(),
 	isBot: int("is_bot", { mode: "boolean" }).notNull(),
+
 	languageCode: text("language_code").notNull(),
+	subscribed: int("subscribed", { mode: "boolean" }).notNull().default(true),
+	acceptTerms: int("accept_terms", { mode: "boolean" }).notNull().default(true),
+	createdAt: int("created_at", { mode: "timestamp" }).default(
+		sql`(unixepoch())`,
+	),
+	updatedAt: int("updated_at", { mode: "timestamp" }).default(
+		sql`(unixepoch())`,
+	),
+});
+
+export const newsTable = sqliteTable("news", {
+	id: int().primaryKey({ autoIncrement: true }),
+	title: text().notNull(),
+	news: text().notNull(),
+	comment: text().notNull(),
+	createdAt: int("created_at", { mode: "timestamp" }).default(
+		sql`(unixepoch())`,
+	),
+	updatedAt: int("updated_at", { mode: "timestamp" }).default(
+		sql`(unixepoch())`,
+	),
 });
