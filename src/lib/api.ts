6c5cd7122b19aeed1ee2c0b7fd290971a2256363
Raphael Elias <raphaeleliass@outlook.com>
2025-09-15 21:53:41 -0300

    feat: add a sendMessage handle to improve reusability

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,3 +4,12 @@ import { telegramToken } from "../constants";
 export const api = axios.create({
 	baseURL: `https://api.telegram.org/bot${telegramToken}`,
 });
+
+export async function sendMessage(chatId: number, text: string) {
+	const { data } = await api.post("/sendMessage", {
+		chat_id: chatId,
+		text: text,
+	});
+
+	return data;
+}
