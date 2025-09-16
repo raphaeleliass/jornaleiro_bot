f89cf4f7fe642badf7e72aed3e7871f9060af8d8
Raphael Elias <raphaeleliass@outlook.com>
2025-09-15 21:52:16 -0300

    refactor: added subscribeUser and adjust on constructors on bindings

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -8,10 +8,15 @@ export class UserRepository {
 
 	constructor(db: DrizzleDB) {
 		this.db = db;
+
+		this.createUser = this.createUser.bind(this);
+		this.findUser = this.findUser.bind(this);
+		this.unsubscribeUser = this.unsubscribeUser.bind(this);
+		this.subscribeUser = this.subscribeUser.bind(this);
 	}
 
-	async findUser({ chatId }: UserTypes) {
-		const user = await this.db
+	async findUser(chatId: number) {
+		const user = this.db
 			.select()
 			.from(userTable)
 			.where(eq(userTable.chatId, chatId))
@@ -20,22 +25,23 @@ export class UserRepository {
 		return user;
 	}
 
-	async registerUser({
+	async createUser({
 		name,
 		chatId,
 		isBot,
 		languageCode,
 		subscribed,
 	}: UserTypes) {
-		const registeredUser = await this.db
+		const createdUser = await this.db
 			.insert(userTable)
 			.values({ name, chatId, isBot, languageCode, subscribed })
-			.returning();
+			.returning()
+			.onConflictDoNothing();
 
-		return registeredUser;
+		return createdUser;
 	}
 
-	async unsubscribeUser({ chatId }: UserTypes) {
+	async unsubscribeUser(chatId: number) {
 		const unsubscribedUser = await this.db
 			.update(userTable)
 			.set({ subscribed: false })
@@ -43,5 +49,15 @@ export class UserRepository {
 			.returning();
 
 		return unsubscribedUser;
+	}
+
+	async subscribeUser(chatId: number) {
+		const subscribeUser = await this.db
+			.update(userTable)
+			.set({ subscribed: true })
+			.where(eq(userTable.chatId, chatId))
+			.returning();
+
+		return subscribeUser;
 	}
 }
