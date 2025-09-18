8e2133fe52ec58377eb3b7d92eb772a1bf065494
Raphael Elias <raphaeleliass@outlook.com>
2025-09-18 09:22:14 -0300

    delete: removed unused files

--- src/lib/logger.ts
+++
@@ -1,8 +0,0 @@
-import { createMiddleware } from "hono/factory";
-
-export const logger = createMiddleware(async (c, next) => {
-	const currDate = new Date().toLocaleTimeString();
-	const path = c.req.path.replace(/handler\/[^/]+/, "handler/*****");
-	console.log(`<-- ${currDate} ${c.req.method} ${path}`);
-	return next();
-});
