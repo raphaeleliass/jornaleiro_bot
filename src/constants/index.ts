28654c6bf2e256e98d6728ed0bbb4bb0b801ee77
Raphael Elias <raphaeleliass@outlook.com>
2025-09-18 09:19:19 -0300

    feat: added cron secret variable

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export const telegramToken = process.env.TELEGRAM_TOKEN;
 export const botSecret = process.env.BOT_SECRET;
 export const geminiApiKey = process.env.GEMINI_API_KEY;
+export const cronSecret = process.env.CRON_SECRET as string;
