c0f0b118734dd3d761780956748fe4e86b35ebeb
Raphael Elias <raphaeleliass@outlook.com>
2025-09-19 19:44:45 -0300

    feat: added summarize route

--- src/routes/botRoute.ts
+++ src/routes/botRoute.ts
@@ -4,6 +4,8 @@ import { botSecret, cronSecret } from "../constants";
 import { db } from "../lib/db";
 import { BotController } from "../modules/bot/bot.controller";
 import { BotService } from "../modules/bot/bot.service";
+import { SummarizeController } from "../modules/summarize/summarize.controller";
+import { SummarizeService } from "../modules/summarize/summarize.service";
 import { UserRepository } from "../modules/user/user.repository";
 import { UserService } from "../modules/user/user.service";
 
@@ -14,5 +16,12 @@ const userService = new UserService(userRepository);
 const botService = new BotService(userService);
 const botController = new BotController(botService);
 
+const summarizeService = new SummarizeService(userRepository);
+const summarizeController = new SummarizeController(summarizeService);
+
 botRoute.post(`/webhook/${botSecret}`, botController.handler);
-botRoute.get("/summarize", bearerAuth({ token: cronSecret }));
+botRoute.get(
+	"/summarize",
+	bearerAuth({ token: cronSecret }),
+	summarizeController.handler,
+);
