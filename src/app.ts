79a49684df69adcb0a3d10165b8c7f3d66e6b536
Raphael Elias <raphaeleliass@outlook.com>
2025-09-19 21:51:28 -0300

    refactor: change app's export

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { logger } from "./lib/utils/logger";
 import { router } from "./routes/router";
 
-export const app = new Hono();
+const app = new Hono();
 
 app.onError((err, c) => {
 	return c.json(err);
@@ -14,3 +14,5 @@ app.get("/", (c) => {
 	return c.text("Hello, Jornaleiro_bot!");
 });
 app.route("/api", router.botRoute);
+
+export default app;
