dee0b927163ab988e9a41853ee1f16cfc40efa61
Raphael Elias <raphaeleliass@outlook.com>
2025-09-19 21:34:13 -0300

    refactor: removed unused constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
 export const telegramToken = process.env.TELEGRAM_TOKEN;
 export const botSecret = process.env.BOT_SECRET;
-export const geminiApiKey = process.env.GEMINI_API_KEY;
 export const cronSecret = process.env.CRON_SECRET as string;
